29c24834de1f3136e06c859834ab12287144c50d
bill donner <billdonner@gmail.com>
2024-06-02 08:18:47 -0500

    Initial Commit

---
+++ qview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qview
+//
+//  Created by bill donner on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qview/qviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  qviewApp.swift
+//  qview
+//
+//  Created by bill donner on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct qviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
